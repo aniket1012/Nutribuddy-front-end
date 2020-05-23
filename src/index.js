a412e9db5c57a77159cc9813e52bbac917cf7067
Aniket <aniketupatel1@gmail.com>
2020-05-23 16:01:20 -0400

    created store and reducer file with test function and case

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 import App from './App';
 
 import { createStore } from 'redux'
-// import reducer from './reducers/reducer'
+import reducer from './reducers/reducer'
 
-function reducer(prevState, action) {
-   // whatever is returned from reducer becomes state  
-   //called on initialization to initialize state
-
-    
-}
 
 const store = createStore(reducer)
+console.log('BEFORE', store.getState());
+
+
+store.dispatch({type: 'LIKE'})
+
+console.log('AFTER', store.getState());
 
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,8 +1,22 @@
-function reducer() {
-    console.log('called reducer');
-    
+const defaultState = {
+    count: 0,
 }
 
-export {
-    reducer
+function reducer(prevState = defaultState, action) {
+    console.log('ACTION', action)
+    // whatever is returned from reducer becomes state  
+    //called on initialization to initialize state
+    switch (action.type) {
+        case 'LIKE':
+            return {
+                ...prevState, count: prevState.count + 1
+            }
+
+
+            default:
+                return prevState
+
+    }
 }
+
+export default reducer
