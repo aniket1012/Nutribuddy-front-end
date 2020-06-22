66e48f3a5de658e95664e36ee6a397298f793c3c
Aniket <aniketupatel1@gmail.com>
2020-06-22 15:35:52 -0400

    adding inputs and layout to bmr calc

---
+++ src/Components/MacroCalc.js
@@ -0,0 +1,20 @@
+import react from 'react'
+
+import React, { Component } from 'react';
+
+class MacroCalc extends Component {
+    render() {
+        return (
+            <div className='macro_calc'>
+               <h2> BMR & MacroCalc</h2>
+                <div className='inputs'>
+                    <input></input>
+                    <input></input>
+                </div>
+             
+            </div>
+        );
+    }
+}
+
+export default MacroCalc;
--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react'
+import MacroCalc from '../Components/MacroCalc'
 
 class MainContainer extends Component {
     render() {
         return (
-            <div style={{backgroundColor: 'blue'}}>
-                MainContainer
+            <div className='home_container'>
+                <MacroCalc/>
+                <div className='test'>div 2</div>
             </div>
         )
     }
 }
 
 export default MainContainer
-
-// import React from 'react';
-// import Typography from '@material-ui/core/Typography';
-// import Container from '@material-ui/core/Container';
-
-// export default function MainContainer() {
-//   return (
-//       <Container height='100vh' maxWidth='100vw' style={{padding: '0px'}}>
-//         <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
-//             MainContainer
-//         </Typography>
-//       </Container>
-//   );
-// }
