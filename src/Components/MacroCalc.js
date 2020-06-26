7cb553e34ce807499e844e56012ec43fc095244b
Aniket <aniketupatel1@gmail.com>
2020-06-26 16:49:37 -0400

    added new color to navbar

--- src/Components/MacroCalc.js
+++ src/Components/MacroCalc.js
@@ -11,8 +11,8 @@ class MacroCalc extends Component {
         return (
             <div className='macro_calc'>
                 <h2> BMR & MacroCalc</h2>
-                <p> To calculate your daily calorie and macro goals, simply fill out the information on the calculator below! </p>
                 <div className='inputs_container'>
+                    <h5> To calculate your daily calorie and macro goals, simply fill out the information on the calculator below! </h5>
                             <br/>
                             <h4>Choose your Gender</h4>
                             <select  class= 'input' name='' placeholder='Gender'>
@@ -21,7 +21,7 @@ class MacroCalc extends Component {
                                 <option value='female'm>Female</option>
                             </select>
                             <h4>Enter Age</h4>
-                            <input  class='input' type='text' name='' placeholder='Age'></input>
+                            <input  class='input' type='number' name='' placeholder='Age'></input>
                             <h4>Enter Weight</h4>
                             <input class='input' type='number' name='' placeholder='Weight (lbs)'></input>
                             <h4>Exercise Level</h4>
@@ -31,7 +31,7 @@ class MacroCalc extends Component {
                                 <option value='female'm>Moderate (3x-4x Per Week)</option>
                                 <option value='female'm>Active(5x+ Per Week)</option>
                             </select>
-                            <label>Primary Goal</label>
+                            <h4>Primary Goal</h4>
                             <select  class= 'input' name='' placeholder='Primary Goal'>
                                 <option value=''>Select</option>
                                 <option value='male'>Loose Weight</option>
