6ca8738dd85883a70eb05c320325cd5b0b6f6632
Aniket <aniketupatel1@gmail.com>
2020-07-19 15:56:19 -0400

    added search inputs and styled them

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,10 +3,11 @@ import React from 'react';
 const SearchBar = () => {
     return (
         <div className='search_bar'>
-            <div>
-                <input/>
-                <input/>
-                <input/>
+            <div className='create_search_inputs_container'>
+                <input id='quantity' placeholder='Qty'/>
+                <input id='unit' placeholder='Unit'/>
+                <input id='search_ing' placeholder='Search Ingredients'/>
+                <button id='search_submit'>Search</button>
             </div>
         </div>
     );
