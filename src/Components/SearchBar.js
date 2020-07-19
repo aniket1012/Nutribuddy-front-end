593937f2f3df0e0fb947f2330287e9f9a34cd03b
Aniket <aniketupatel1@gmail.com>
2020-07-19 16:04:00 -0400

    added state to capure inputs

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return (
-        <div className='search_bar'>
-            <div className='create_search_inputs_container'>
-                <input id='quantity' placeholder='Qty'/>
-                <input id='unit' placeholder='Unit'/>
-                <input id='search_ing' placeholder='Search Ingredients'/>
-                <button id='search_submit'>Search</button>
+class SearchBar extends Component {
+
+    state = {
+        quantity: null,
+        unit: null,
+        searchTerm: null,
+
+        
+    }
+
+    render() {
+        return (
+            <div className='search_bar'>
+                <div className='create_search_inputs_container'>
+                    <input id='quantity' placeholder='Qty' type='number'/>
+                    <input id='unit' placeholder='Unit'/>
+                    <input id='search_ing' placeholder='Search Ingredients 🔍'/>
+                    <button class='search_submit'>Search</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+
+    }
 };
 
 export default SearchBar;
+
