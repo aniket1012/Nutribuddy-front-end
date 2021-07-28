ec762b5d6405fbdd0e98b056797e061bbc3665fa
Aniket <aniketupatel1@gmail.com>
2021-07-28 13:59:12 -0400

    added app bar with elevation scrolling from MUI

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -2,12 +2,31 @@ import React from 'react'
 import { 
   AppBar,
   Toolbar, 
+  useScrollTrigger
 } from '@material-ui/core'
+
+
+function ElevationScroll(props) {
+  const { children} = props;
+ 
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 
 function Header() {
   return (
-    <AppBar>
-      <Toolbar>NutriBuddy</Toolbar>
+    <AppBar position='fixed'>
+      <ElevationScroll>
+        <Toolbar>NutriBuddy</Toolbar>
+      </ElevationScroll>
     </AppBar>
   )
 }
