ebb972f1a7ccb32526efc98d7b05aac3d3de2f66
Aniket <aniketupatel1@gmail.com>
2021-07-30 14:15:43 -0400

    added mediaQuery hook to remove tabs on page downsize to add a custom drawer for easier use

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { 
   AppBar,
   Toolbar, 
@@ -6,8 +6,10 @@ import {
   Typography,
   Tabs,
   Tab,
-  Button
+  Button,
+  useMediaQuery
 } from '@material-ui/core'
+import { useTheme } from "@material-ui/core/styles";
 import { Link } from 'react-router-dom';
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -34,8 +36,15 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '3em'
   },
   logo: {
-    height: '7em',
+    height: '8em',
     paddingLeft: '10px'
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: 'transparent'
+
+    }
   },
   tabContainer: {
     marginLeft: 'auto'
@@ -51,61 +60,84 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: '50px',
     marginRight: '25px',
     height: '45px',
-    
-
   }
-
-
 }))
 
 function Header() {
   const classes = useStyles()
   const [value, setValue] = useState(0)
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down('md'))
 
   const handleChange = (e, value) => {
     setValue(value)
   }
 
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/createmeal" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/mymeals" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/login" && value !== 3) {
+      setValue(3);
+    } 
+  }, [value])
 
+  const tabs = (
+    <React.Fragment>
+      <Tabs
+        value={value}
+        className={classes.tabContainer}
+        onChange={handleChange}
+      >
+        <Tab className={classes.tab} label="Home" component={Link} to="/" />
+        <Tab
+          className={classes.tab}
+          label="Create Meal"
+          component={Link}
+          to="/createmeal"
+        />
+        <Tab
+          className={classes.tab}
+          label="My Meals"
+          component={Link}
+          to="/mymeals"
+        />
+        <Tab
+          className={classes.tab}
+          label="Login"
+          component={Link}
+          to="/login"
+        />
+      </Tabs>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        component={Link}
+        to="signup"
+      >
+        Sign Up
+      </Button>
+    </React.Fragment>
+  );
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
           <Toolbar disableGutters>
-            <img src={logo} alt="logo" className={classes.logo}/>
-            <Tabs 
-              value={value} 
-              className={classes.tabContainer}
-              onChange={handleChange}
+            <Button 
+              component={Link} 
+              to='/'
+              className={classes.logoContainer}
+              onClick={()=> setValue(0)}
+              disableRipple
             >
-              <Tab 
-                className={classes.tab} 
-                label="Home" 
-                component={Link}
-                to='/'
-              />
-              <Tab 
-                className={classes.tab} 
-                label="Create Meal"
-                component={Link}
-                to='/createmeal' 
-              />
-              <Tab 
-                className={classes.tab} 
-                label="My Meals" 
-                component={Link}
-                to='/mymeals'
-              />
-            </Tabs>
-            <Button
-              variant="contained"
-              color="secondary"
-              className={classes.button}
-              component={Link}
-              to='signup'
-            >
-              Sign Up
+              <img src={logo} alt="logo" className={classes.logo}/>
             </Button>
+            {matches ? null : tabs}
           </Toolbar>
         </AppBar>
       </ElevationScroll>
