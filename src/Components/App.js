6ce84284cb5e5c4ee27ce2aef450b20b455608e8
Aniket <aniketupatel1@gmail.com>
2021-08-10 13:54:56 -0400

    added bare bones to macro calc, added title, inputs not styled

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from './UI/Theme'
-
+import Landing from './Landing';
 
 import Header from './UI/Header'
 
@@ -12,7 +12,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <Header/>
         <Switch>
-          <Route exact path='/' component={() => <div>HOME</div>}></Route>
+          <Route exact path='/' component={Landing}></Route>
           <Route exact path='/createmeal' component={() => <div>CREATE MEAL</div>}></Route>
           <Route exact path='/mymeals' component={() => <div>MY MEALS</div>}></Route>
           <Route exact path='/login' component={() => <div>LOGIN</div>}></Route>
---
+++ src/Components/Landing.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react'
+import { 
+    Grid,
+    Typography,
+    Button,
+    Paper,
+    TextField,
+    Switch
+ } from '@material-ui/core'
+ import { Link } from 'react-router-dom';
+ import { makeStyles } from "@material-ui/core/styles";
+
+
+ const useStyles = makeStyles((theme) => ({
+   landingContainer: {
+
+   },
+   landingRight:{
+    // backgroundColor: 'blue',
+    color: theme.palette.common.lightGrey
+   }
+ }));
+
+function Landing() {
+    const classes = useStyles()
+
+    const [state, setState] = React.useState({
+      checkedA: true,
+      checkedB: true,
+    });
+
+    const handleChange = (event) => {
+      setState({ ...state, [event.target.name]: event.target.checked });
+    };
+
+    return (
+      <Grid container className={classes.landingContainer} spacing={2}>
+        <Grid item xs={12} className={classes.landingRight}>
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <Typography variant="h3">MacroCaclulator</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="h6">
+                Enter Credentials to Find BaseLine
+              </Typography>
+            </Grid>
+            <Grid item container direction="column" spacing={2}>
+              <Grid item>
+                <TextField id="filled-basic" label="Age" variant="filled" />
+              </Grid>
+              <Grid item>
+                <TextField id="filled-basic" label="Sex" variant="filled" />
+              </Grid>
+              <Grid item>
+                <TextField id="filled-basic" label="Height" variant="filled" />
+              </Grid>
+              <Grid item>
+                <TextField id="filled-basic" label="Weight" variant="filled" />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="filled-basic"
+                  label="Activity Level"
+                  variant="filled"
+                />
+              </Grid>
+              <Grid item>
+                <TextField id="filled-basic" label="Goal" variant="filled" />
+              </Grid>
+            </Grid>
+            <Grid item>Button</Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+}
+
+export default Landing
